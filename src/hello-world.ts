c5e461f7234aac073640bbab6e44c5e165bfdec4
Jan <none>
2018-10-19 14:15:13 +0200

    initial commit

---
+++ src/hello-world.ts
@@ -0,0 +1,14 @@
+﻿import VssControls = require('VSS/Controls');
+
+import _ = require('lodash');
+
+VSS.ready(function() {
+    console.log("Hello World!");
+    // just calling a method to see whether it works
+    console.log(VssControls.getId());
+
+    // calling a method from lodash to see whether it works
+    _.forEach([2,3,4], val => {
+        console.log(val);
+    });
+});
